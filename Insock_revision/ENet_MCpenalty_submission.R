c7e284acbccbf9589193f8ad80882088f14fa0ed
Ubuntu <ubuntu@ip-10-144-69-99.ec2.internal>
2013-02-07 01:06:39 +0000

    bugs fixed

--- Insock_revision/ENet_MCpenalty_submission.R
+++ Insock_revision/ENet_MCpenalty_submission.R
@@ -1,4 +1,4 @@
-### ENet Molecular Feature only 
+### Elastic Net Molecular Feature only 
 
 ###################################################
 ### step 1: loadLibraries
@@ -24,12 +24,14 @@ modelClassFile = ("~/Federation/Insock_revision/myEnetCoxModel.R")
 source(modelClassFile)
 source(modelClassFile0)
 
-modelClassFile1 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_cancerCensus.R")
-modelClassFile2 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_marginalAssociation.R")
-modelClassFile3 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_metabricClustering.R")
-modelClassFile4 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_topvaryingHiggins.R")
-modelClassFile5 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_topvarying.R")
-modelClassFile6 = ("~/DrugResponse/survival_analysis/Insock_revision/MC_penalty/MC_ExpCNV_masp.R")
+modelClassFile1 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_cancerCensus.R")
+modelClassFile2 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_marginalAssociation.R")
+modelClassFile3 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_metabricClustering.R")
+modelClassFile4 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_topvaryingHiggins.R")
+modelClassFile5 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_topvarying.R")
+modelClassFile6 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_masp.R")
+modelClassFile7 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_OncomapDx.R")
+modelClassFile8 = ("~/Federation/Insock_revision/MC_penalty/MC_ExpCNV_Mamaprint.R")
 
 source(modelClassFile1)
 source(modelClassFile2)
@@ -37,11 +39,13 @@ source(modelClassFile3)
 source(modelClassFile4)
 source(modelClassFile5)
 source(modelClassFile6)
+source(modelClassFile7)
+source(modelClassFile8)
 
 ###################################################
 ### step 4: trainModel
 ###################################################
-# ENet Grid Setting
+# Elastic Net Grid Setting
 alphas = seq(0.05,0.95,by=0.05)
 lambdas <- exp(seq(-5, 2, length = 100))
 
@@ -70,10 +74,20 @@ Topvaring$customTrain(trainingData$exprData,trainingData$copyData,trainingData$c
 trainPredictions5 <- Topvaring$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
 testPredictions5 <- Topvaring$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
 
-Masp <- MC_ExpCNV_topvarying$new()
+Masp <- MC_ExpCNV_masp$new()
 Masp$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
 trainPredictions6 <- Masp$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
 testPredictions6 <- Masp$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
+
+OncomapDx <- MC_ExpCNV_OncomapDx$new()
+OncomapDx$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
+trainPredictions7 <- OncomapDx$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
+testPredictions7 <- OncomapDx$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
+
+Mammaprint <- MC_ExpCNV_Mamaprint$new()
+Mammaprint$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
+trainPredictions8 <- Mammaprint$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
+testPredictions8 <- Mammaprint$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
 
 
 ###################################################
@@ -85,6 +99,8 @@ trainPerformance3 <- SurvivalModelPerformance$new(as.numeric(trainPredictions3),
 trainPerformance4 <- SurvivalModelPerformance$new(as.numeric(trainPredictions4), trainingData$clinicalSurvData[rownames(trainPredictions4),])
 trainPerformance5 <- SurvivalModelPerformance$new(as.numeric(trainPredictions5), trainingData$clinicalSurvData[rownames(trainPredictions5),])
 trainPerformance6 <- SurvivalModelPerformance$new(as.numeric(trainPredictions6), trainingData$clinicalSurvData[rownames(trainPredictions6),])
+trainPerformance7 <- SurvivalModelPerformance$new(as.numeric(trainPredictions7), trainingData$clinicalSurvData[rownames(trainPredictions7),])
+trainPerformance8 <- SurvivalModelPerformance$new(as.numeric(trainPredictions8), trainingData$clinicalSurvData[rownames(trainPredictions8),])
 
 print(trainPerformance1$getExactConcordanceIndex())
 print(trainPerformance2$getExactConcordanceIndex())
@@ -92,6 +108,8 @@ print(trainPerformance3$getExactConcordanceIndex())
 print(trainPerformance4$getExactConcordanceIndex())
 print(trainPerformance5$getExactConcordanceIndex())
 print(trainPerformance6$getExactConcordanceIndex())
+print(trainPerformance7$getExactConcordanceIndex())
+print(trainPerformance8$getExactConcordanceIndex())
 
 testPerformance1 <- SurvivalModelPerformance$new(as.numeric(testPredictions1), testingData$clinicalSurvData[rownames(testPredictions1),])
 testPerformance2 <- SurvivalModelPerformance$new(as.numeric(testPredictions2), testingData$clinicalSurvData[rownames(testPredictions2),])
@@ -99,6 +117,8 @@ testPerformance3 <- SurvivalModelPerformance$new(as.numeric(testPredictions3), t
 testPerformance4 <- SurvivalModelPerformance$new(as.numeric(testPredictions4), testingData$clinicalSurvData[rownames(testPredictions4),])
 testPerformance5 <- SurvivalModelPerformance$new(as.numeric(testPredictions5), testingData$clinicalSurvData[rownames(testPredictions5),])
 testPerformance6 <- SurvivalModelPerformance$new(as.numeric(testPredictions6), testingData$clinicalSurvData[rownames(testPredictions6),])
+testPerformance7 <- SurvivalModelPerformance$new(as.numeric(testPredictions7), testingData$clinicalSurvData[rownames(testPredictions7),])
+testPerformance8 <- SurvivalModelPerformance$new(as.numeric(testPredictions8), testingData$clinicalSurvData[rownames(testPredictions8),])
 
 print(testPerformance1$getExactConcordanceIndex())
 print(testPerformance2$getExactConcordanceIndex())
@@ -106,6 +126,8 @@ print(testPerformance3$getExactConcordanceIndex())
 print(testPerformance4$getExactConcordanceIndex())
 print(testPerformance5$getExactConcordanceIndex())
 print(testPerformance6$getExactConcordanceIndex())
+print(testPerformance7$getExactConcordanceIndex())
+print(testPerformance8$getExactConcordanceIndex())
 
 
 
@@ -119,12 +141,16 @@ myGeneList3 = "Metabric Clustering + Clinical"
 myGeneList4 = "Higgins + Clinical" 
 myGeneList5 = "Top-varying + Clinical" 
 myGeneList6 = "MASP + Clinical" 
+myGeneList7 = "OncomapDx + Clinical" 
+myGeneList8 = "Mammaprint + Clinical" 
 
 
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical CancerCensus", trainedModel=CancerCensus,rFiles=list(modelClassFile1,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList1)
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical Mariginal Association", trainedModel=MarginalAssociation,rFiles=list(modelClassFile2,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList2)
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical MetabricClustering", trainedModel=MetabricClustering,rFiles=list(modelClassFile3,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList3)
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical TopvaringHiggins", trainedModel=TopvaringHiggins,rFiles=list(modelClassFile4,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList4)
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical Topvaring", trainedModel=Topvaring,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList5)
-submitCompetitionModel_micmaTrained_InSock(modelName = "ENet without penalty with expr +copy + clinical Masp", trainedModel=Masp,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList6)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical CancerCensus", trainedModel=CancerCensus,rFiles=list(modelClassFile1,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList1)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical Mariginal Association", trainedModel=MarginalAssociation,rFiles=list(modelClassFile2,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList2)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical MetabricClustering", trainedModel=MetabricClustering,rFiles=list(modelClassFile3,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList3)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical TopvaringHiggins", trainedModel=TopvaringHiggins,rFiles=list(modelClassFile4,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList4)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical Topvaring", trainedModel=Topvaring,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList5)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical Masp", trainedModel=Masp,rFiles=list(modelClassFile6,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList6)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical OncomapDx", trainedModel=Masp,rFiles=list(modelClassFile7,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList7)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Elastic Net without penalty with expr + copy + clinical Mammaprint", trainedModel=Masp,rFiles=list(modelClassFile8,modelClassFile,modelClassFile0), algorithm = "enet", geneList= myGeneList8)
 
--- Insock_revision/Lasso_M_submission.R
+++ Insock_revision/Lasso_M_submission.R
@@ -23,12 +23,14 @@ modelClassFile0 = ("~/Federation/Insock_revision/mapper.R")
 modelClassFile = ("~/Federation/Insock_revision/myEnetCoxModel.R")
 source(modelClassFile)
 
-modelClassFile1 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_cancerCensus.R")
-modelClassFile2 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_marginalAssociation.R")
-modelClassFile3 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_metabricClustering.R")
-modelClassFile4 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_topvaryingHiggins.R")
-modelClassFile5 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_topvarying.R")
-modelClassFile6 = ("~/DrugResponse/survival_analysis/Insock_revision/M/M_ExpCNV_masp.R")
+modelClassFile1 = ("~/Federation/Insock_revision/M/M_ExpCNV_cancerCensus.R")
+modelClassFile2 = ("~/Federation/Insock_revision/M/M_ExpCNV_marginalAssociation.R")
+modelClassFile3 = ("~/Federation/Insock_revision/M/M_ExpCNV_metabricClustering.R")
+modelClassFile4 = ("~/Federation/Insock_revision/M/M_ExpCNV_topvaryingHiggins.R")
+modelClassFile5 = ("~/Federation/Insock_revision/M/M_ExpCNV_topvarying.R")
+modelClassFile6 = ("~/Federation/Insock_revision/M/M_ExpCNV_masp.R")
+modelClassFile7 = ("~/Federation/Insock_revision/M/M_ExpCNV_OncomapDx.R")
+modelClassFile8 = ("~/Federation/Insock_revision/M/M_ExpCNV_Mamaprint.R")
 
 source(modelClassFile1)
 source(modelClassFile2)
@@ -36,6 +38,8 @@ source(modelClassFile3)
 source(modelClassFile4)
 source(modelClassFile5)
 source(modelClassFile6)
+source(modelClassFile7)
+source(modelClassFile8)
 
 ###################################################
 ### step 4: trainModel
@@ -69,10 +73,20 @@ Topvaring$customTrain(trainingData$exprData,trainingData$copyData,trainingData$c
 trainPredictions5 <- Topvaring$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
 testPredictions5 <- Topvaring$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
 
-Masp <- M_ExpCNV_topvarying$new()
+Masp <- M_ExpCNV_masp$new()
 Masp$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
 trainPredictions6 <- Masp$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
 testPredictions6 <- Masp$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
+
+OncomapDx <- M_ExpCNV_OncomapDx$new()
+OncomapDx$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
+trainPredictions7 <- OncomapDx$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
+testPredictions7 <- OncomapDx$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
+
+Mammaprint <- M_ExpCNV_Mamaprint$new()
+Mammaprint$customTrain(trainingData$exprData,trainingData$copyData,trainingData$clinicalFeaturesData,trainingData$clinicalSurvData, alpha = alphas,lambda = lambdas)
+trainPredictions8 <- Mammaprint$customPredict(trainingData$exprData, trainingData$copyData, trainingData$clinicalFeaturesData)
+testPredictions8 <- Mammaprint$customPredict(testingData$exprData, testingData$copyData, testingData$clinicalFeaturesData)
 
 
 ###################################################
@@ -84,6 +98,8 @@ trainPerformance3 <- SurvivalModelPerformance$new(as.numeric(trainPredictions3),
 trainPerformance4 <- SurvivalModelPerformance$new(as.numeric(trainPredictions4), trainingData$clinicalSurvData[rownames(trainPredictions4),])
 trainPerformance5 <- SurvivalModelPerformance$new(as.numeric(trainPredictions5), trainingData$clinicalSurvData[rownames(trainPredictions5),])
 trainPerformance6 <- SurvivalModelPerformance$new(as.numeric(trainPredictions6), trainingData$clinicalSurvData[rownames(trainPredictions6),])
+trainPerformance7 <- SurvivalModelPerformance$new(as.numeric(trainPredictions7), trainingData$clinicalSurvData[rownames(trainPredictions7),])
+trainPerformance8 <- SurvivalModelPerformance$new(as.numeric(trainPredictions8), trainingData$clinicalSurvData[rownames(trainPredictions8),])
 
 print(trainPerformance1$getExactConcordanceIndex())
 print(trainPerformance2$getExactConcordanceIndex())
@@ -91,6 +107,8 @@ print(trainPerformance3$getExactConcordanceIndex())
 print(trainPerformance4$getExactConcordanceIndex())
 print(trainPerformance5$getExactConcordanceIndex())
 print(trainPerformance6$getExactConcordanceIndex())
+print(trainPerformance7$getExactConcordanceIndex())
+print(trainPerformance8$getExactConcordanceIndex())
 
 testPerformance1 <- SurvivalModelPerformance$new(as.numeric(testPredictions1), testingData$clinicalSurvData[rownames(testPredictions1),])
 testPerformance2 <- SurvivalModelPerformance$new(as.numeric(testPredictions2), testingData$clinicalSurvData[rownames(testPredictions2),])
@@ -98,6 +116,8 @@ testPerformance3 <- SurvivalModelPerformance$new(as.numeric(testPredictions3), t
 testPerformance4 <- SurvivalModelPerformance$new(as.numeric(testPredictions4), testingData$clinicalSurvData[rownames(testPredictions4),])
 testPerformance5 <- SurvivalModelPerformance$new(as.numeric(testPredictions5), testingData$clinicalSurvData[rownames(testPredictions5),])
 testPerformance6 <- SurvivalModelPerformance$new(as.numeric(testPredictions6), testingData$clinicalSurvData[rownames(testPredictions6),])
+testPerformance7 <- SurvivalModelPerformance$new(as.numeric(testPredictions7), testingData$clinicalSurvData[rownames(testPredictions7),])
+testPerformance8 <- SurvivalModelPerformance$new(as.numeric(testPredictions8), testingData$clinicalSurvData[rownames(testPredictions8),])
 
 print(testPerformance1$getExactConcordanceIndex())
 print(testPerformance2$getExactConcordanceIndex())
@@ -105,6 +125,8 @@ print(testPerformance3$getExactConcordanceIndex())
 print(testPerformance4$getExactConcordanceIndex())
 print(testPerformance5$getExactConcordanceIndex())
 print(testPerformance6$getExactConcordanceIndex())
+print(testPerformance7$getExactConcordanceIndex())
+print(testPerformance8$getExactConcordanceIndex())
 
 
 
@@ -118,11 +140,15 @@ myGeneList3 = "Metabric Clustering"
 myGeneList4 = "Higgins" 
 myGeneList5 = "Top-varying" 
 myGeneList6 = "MASP" 
+myGeneList7 = "OncomapDx" 
+myGeneList8 = "Mammaprint" 
 
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy CancerCensus", trainedModel=CancerCensus,rFiles=list(modelClassFile1,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList1)
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy Mariginal Association", trainedModel=MarginalAssociation,rFiles=list(modelClassFile2,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList2)
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy MetabricClustering", trainedModel=MetabricClustering,rFiles=list(modelClassFile3,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList3)
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy TopvaringHiggins", trainedModel=TopvaringHiggins,rFiles=list(modelClassFile4,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList4)
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy Topvaring", trainedModel=Topvaring,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList5)
-submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr +copy Masp", trainedModel=Masp,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList6)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy CancerCensus", trainedModel=CancerCensus,rFiles=list(modelClassFile1,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList1)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy Mariginal Association", trainedModel=MarginalAssociation,rFiles=list(modelClassFile2,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList2)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy MetabricClustering", trainedModel=MetabricClustering,rFiles=list(modelClassFile3,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList3)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy TopvaringHiggins", trainedModel=TopvaringHiggins,rFiles=list(modelClassFile4,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList4)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy Topvaring", trainedModel=Topvaring,rFiles=list(modelClassFile5,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList5)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy Masp", trainedModel=Masp,rFiles=list(modelClassFile6,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList6)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy OncomapDx", trainedModel=OncomapDx,rFiles=list(modelClassFile7,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList7)
+submitCompetitionModel_micmaTrained_InSock(modelName = "Lasso without penalty with expr + copy Mammaprint", trainedModel=Mammaprint,rFiles=list(modelClassFile8,modelClassFile,modelClassFile0), algorithm = "lasso", geneList= myGeneList8)
 
